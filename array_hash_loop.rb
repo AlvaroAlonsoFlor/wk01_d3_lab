475d866e5539573d1e40d7a73bfd0fb21ddf399d
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-01 15:19:58 +0100

    First and second function

---
+++ array_hash_loop.rb
@@ -0,0 +1,20 @@
+# Write a function that takes has two parameters, array1 and array2
+# It should return the sum of the array lengths
+
+def add_array_lengths(array1, array2)
+
+  return array1.length() + array2.length()
+
+end
+
+# Write a function that has a parameter named numbers
+# It should return the sum value of
+#an array passed as the argument
+
+def some_array(numbers)
+  total = 0
+  for number in numbers
+    total += number
+  end
+  return total
+end
